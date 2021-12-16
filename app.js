d5316265b61cfd4affe4c7bc62aff52c3763eb99
Ken Harrison <63295963+ktreharrison@users.noreply.github.com>
2021-12-16 16:29:42 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Ken, this is you coming theCreate Continuous Delivery Pipeline with AWS'));
+app.get('/', (req, res) => res.send('Hello Ken, this is you coming the Create Continuous Delivery Pipeline with AWS. This is a new sentence'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
